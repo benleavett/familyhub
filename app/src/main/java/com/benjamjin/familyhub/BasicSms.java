4536129c4e8e64e0a3ad54464b50e7a5aba030e9
Ben (personal) <benjamin@Bens-MacBook-Air.local>
2017-08-08 18:55:42 -0700

    First commit - basic message viewing

---
+++ app/src/main/java/com/benjamjin/familyhub/BasicSms.java
@@ -0,0 +1,15 @@
+package com.benjamjin.familyhub;
+
+/**
+ * Created by benjamin on 08/08/2017.
+ */
+
+class BasicSms {
+    protected String senderAddress;
+    protected String timestampSent;
+    protected String body;
+
+    public String toString() {
+        return String.format("%s, %s, %s", timestampSent, senderAddress, body);
+    }
+}
