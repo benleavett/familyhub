bcc8a19b9b4c783f3113c0384aa0bf0e99dadda1
Ben Leavett <benleavett@gmail.com>
2017-09-30 13:21:02 -0700

    Deploy script now auto-bumps version and checks on right branch

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
-if [ $# -eq 0 ]
+function report_failed_deploy() {
+	echo "> Deploy failed"
+	exit 1
+}
+
+if [ $# -gt 1 ]
   then
-    echo "No arguments supplied"
+    echo "> Invalid arguments supplied"
 else
-    name=$1
-    echo "Creating new release $name"
+	# Make sure we're on the right branch and at HEAD
     cd ~/src/familyhub
-    git tag $name
-    git push github $name
+	git checkout master
+	if [ $? -ne 0 ]
+    	then
+    		report_failed_deploy
+	fi
+	git fetch github
+	git rebase github/master
+	if [ $? -ne 0 ]
+    	then 
+	    	report_failed_deploy
+	fi
+
+	#Generate new version code by incrementing last one
+    old_v=`cat version_code`
+    let "new_v=old_v+1"
+
+    #Tag release
+    if [ $# -eq 1 ]
+    	then
+    	    tag_name=v$new_v-$1
+    else
+    	tag_name=v$new_v
+	fi
+    echo "> Creating new release $tag_name (previous was $old_v)"
+	git tag $tag_name
+    #git push github $tag_name
+    if [ $? -ne 0 ]
+    	then 
+	    	report_failed_deploy
+	else
+		echo $new_v >> version_code
+		git add version_code
+		git commit -m "Bumping version to $new_v"
+		#git push github master
+
+		if [ $? -eq 0 ]
+    		then
+    			echo "> $tag_name pushed and version updated in repo"
+		fi
+	fi
 fi
